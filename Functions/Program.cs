2e04ca258693a5a6fc7f6c95c4eb64927bd59f05
Sinitsa <sinitsammi@gmail.com>
2024-12-08 18:25:46 +0300

    Program

---
+++ Functions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Ваше число: " + ReadInt());
+        }
+
+        static private int ReadInt()
+        {
+            bool IsTakeNumber = false;
+            int playerNumber = 0;
+
+            while (IsTakeNumber == false)
+            {
+                Console.Write("Введите число: ");
+                string playerInput = Console.ReadLine();                
+                IsTakeNumber = int.TryParse(playerInput, out playerNumber);
+
+                if (IsTakeNumber == false)
+                    Console.WriteLine("Ошибка ввода.");                
+            }
+
+            return playerNumber;
+        }
+    }
+}
