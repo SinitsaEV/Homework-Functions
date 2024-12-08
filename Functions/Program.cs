b5e299ef5f302a35f0305e24ebe0e00d645a84a3
Sinitsa <sinitsammi@gmail.com>
2024-12-08 20:01:44 +0300

    Program

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -7,27 +7,49 @@ namespace Functions
     {
         static void Main(string[] args)
         {
+            int maxHealth = 100;
+            int playerHealth;
+            char healthSymbol = '#';
+
+            int maxMana = 100;
+            int playerMana;
+            char manaSymbol = '@';
+
+            int lengthBar = 10;
+
             Console.OutputEncoding = Encoding.Unicode;
 
-            Console.WriteLine("Ваше число: " + ReadInt());
+            Console.Write("Введите здоровье игрока: ");
+            playerHealth = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите ману игрока: ");
+            playerMana = Convert.ToInt32(Console.ReadLine());
+
+            DrawBar(maxHealth, playerHealth, lengthBar, healthSymbol);
+            DrawBar(maxMana, playerMana, lengthBar, manaSymbol);
         }
 
-        static private int ReadInt()
+        static private void DrawBar(int maxValue, int currentValue, int lengthBar, char barSymbol)
         {
-            bool IsTakeNumber = false;
-            int playerNumber = 0;
-
-            while (IsTakeNumber == false)
+            int hundredPercent = 100;
+            float procent = Convert.ToSingle(currentValue) / maxValue * hundredPercent;
+            int barValue = Convert.ToInt32(procent * lengthBar / hundredPercent);
+            string bar = "";
+            char emptySymbol = ' ';
+            
+            for(int i = 0; i < lengthBar; i++)
             {
-                Console.Write("Введите число: ");
-                string playerInput = Console.ReadLine();                
-                IsTakeNumber = int.TryParse(playerInput, out playerNumber);
-
-                if (IsTakeNumber == false)
-                    Console.WriteLine("Ошибка ввода.");                
+                if(barValue > 0)
+                {
+                    barValue--;
+                    bar += barSymbol;
+                }
+                else
+                {
+                    bar += emptySymbol;
+                }
             }
 
-            return playerNumber;
+            Console.WriteLine('[' + bar + ']');
         }
     }
 }
