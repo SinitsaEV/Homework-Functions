4c7c887480c8a993f121f19ddf21671a64c15d15
Sinitsa <sinitsammi@gmail.com>
2025-01-29 16:56:57 +0300

    Program

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 
 namespace Functions
 {
@@ -7,165 +6,86 @@ namespace Functions
     {
         static void Main(string[] args)
         {
-            const string AddDossierCommand = "1";
-            const string RemoveDossierCommand = "2";
-            const string FindDossiersCommand = "3";
-            const string WriteDossiersCommand = "4";
-            const string ExitCommand = "5";
-
-            string addDossierText = $"{AddDossierCommand} - Добывить досье.\n";
-            string removeDossierText = $"{RemoveDossierCommand} - Удалить досье.\n";
-            string findDossiersText = $"{FindDossiersCommand} - Найти досье по фамилии.\n";
-            string writeDossiersText = $"{WriteDossiersCommand} - Показать все досье.\n";
-            string exitText = $"{ExitCommand} - Выйти из программы.\n";
-            string emptyDossiersText = "Список досье пуст, добавте досье.";
-
-            char symbolSeparating = ' ';
-            char symbolSeparatingDossiers = '-';
-
-            string[] fullNames = new string[] { "Синица Евгений Владимирович", "Бурван Илья Викторович" };
-            string[] posts = new string[] { "Системный администратор", "Безработный" };
-
-            bool isWorking = true;
-
-            Console.OutputEncoding = Encoding.Unicode;
-            Console.InputEncoding = Encoding.Unicode;
-
-            while (isWorking)
+            char playerSymbol = '@';
+            char borderSymbol = '#';
+            char[,] map =
             {
-                Console.WriteLine(addDossierText + removeDossierText + findDossiersText + writeDossiersText + exitText);
-                Console.Write("Введите номер команды: ");
+                {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#' },
+                {'#',' ',' ',' ',' ','#',' ',' ',' ','#',' ',' ',' ',' ','#',' ',' ',' ','#' },                
+                {'#',' ',' ',' ',' ','#',' ','*',' ','#',' ',' ',' ',' ','#',' ','*',' ','#' },                
+                {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ','#' },                
+                {'#',' ','#','#','#','#',' ',' ',' ',' ',' ','#','#','#','#',' ',' ',' ','#' },                
+                {'#',' ',' ',' ',' ',' ',' ','*',' ',' ',' ',' ',' ',' ',' ',' ','*',' ','#' },                
+                {'#',' ','*',' ',' ',' ',' ','#','#',' ',' ','*',' ',' ',' ',' ','#','#','#' },                
+                {'#',' ',' ',' ','*',' ','*','#',' ','#',' ',' ',' ','*',' ','*','#',' ','#' },                
+                {'#',' ',' ',' ',' ',' ',' ','#',' ','#',' ',' ',' ',' ',' ',' ','#',' ','#' },                
+                {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#' },
+            };
 
-                switch (Console.ReadLine())
+            int playerPositionX = 1;
+            int playerPositionY = 1;
+
+            while(true)
+            {                
+                Console.CursorVisible = false;
+
+                DrawMap(map);
+                DrawPlayer(playerPositionX, playerPositionY, playerSymbol);
+
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                HandleInput(pressedKey, ref playerPositionX, ref playerPositionY, map, borderSymbol);
+            }            
+        }
+
+        private static void HandleInput(ConsoleKeyInfo pressedKey, ref int playerPositionX, ref int playerPositionY, char[,] map,char borderSymbor)
+        {
+            int[] direction = GetDirection(pressedKey);
+
+            int nextPlayerX = playerPositionX + direction[0];
+            int nextPlayerY = playerPositionY + direction[1];
+
+            if (map[nextPlayerY, nextPlayerX] != borderSymbor)
+            {
+                playerPositionX = nextPlayerX;
+                playerPositionY = nextPlayerY;
+            }
+        }
+
+        private static int[] GetDirection(ConsoleKeyInfo pressedKey)
+        {
+            int[] direction = { 0, 0 };
+
+            if (pressedKey.Key == ConsoleKey.UpArrow)
+                direction[1] = -1;
+            else if(pressedKey.Key == ConsoleKey.DownArrow)
+                direction[1] = 1;
+            else if(pressedKey.Key == ConsoleKey.LeftArrow)
+                direction[0] = -1;
+            else if(pressedKey.Key == ConsoleKey.RightArrow)
+                direction[0] = 1;
+            
+            return direction;
+        }
+
+        private static void DrawMap(char[,] map)
+        {
+            Console.Clear();
+
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
                 {
-                    case AddDossierCommand:
-                        AddDossier(ref fullNames, ref posts);
-                        break;
-
-                    case RemoveDossierCommand:
-                        RemoveDossier(ref fullNames, ref posts, emptyDossiersText);
-                        break;
-
-                    case FindDossiersCommand:
-                        FindDossierByLastName(fullNames, posts, symbolSeparating, emptyDossiersText);
-                        break;
-
-                    case WriteDossiersCommand:
-                        WriteDossiers(fullNames, posts, symbolSeparatingDossiers, emptyDossiersText);
-                        break;
-
-                    case ExitCommand:
-                        isWorking = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Неверная команда.");
-                        break;
+                    Console.Write(map[i, j]);
                 }
+
+                Console.WriteLine();
             }
         }
 
-        private static void AddDossier(ref string[] fullNames, ref string[] posts)
+        private static void DrawPlayer(int playerPositionX,int playerPositionY,char playerSymbol)
         {
-            Console.Write("Введите ФИО. ");
-            AddElementToArray(ref fullNames, Console.ReadLine());
-            Console.Write("Введите должность. ");
-            AddElementToArray(ref posts, Console.ReadLine());
-        }
-
-        private static void AddElementToArray(ref string[] array, string element)
-        {
-            IncreaseArray(ref array);
-            array[array.Length - 1] = element;
-        }
-
-        private static void IncreaseArray(ref string[] array)
-        {
-            string[] temp = new string[array.Length + 1];
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                temp[i] = array[i];
-            }
-
-            array = temp;
-        }
-
-        private static void RemoveDossier(ref string[] fullNames, ref string[] posts, string emptyText)
-        {
-            if (fullNames.Length == 0)
-            {
-                Console.WriteLine(emptyText);
-                return;
-            }
-
-            Console.Write("Введите номер досье: ");
-            int removeElementIndex = Convert.ToInt32(Console.ReadLine()) - 1;
-            RemoveArrayElement(ref fullNames, removeElementIndex);
-            RemoveArrayElement(ref posts, removeElementIndex);
-        }
-
-        private static void RemoveArrayElement(ref string[] array,int removeElementIndex)
-        {
-            string[] tempArray = new string[array.Length - 1];
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (i < removeElementIndex)
-                {
-                    tempArray[i] = array[i];
-                }
-                else if (i > removeElementIndex)
-                {
-                    tempArray[i - 1] = array[i];
-                }
-            }
-
-            array = tempArray;
-        }
-
-        private static void WriteDossiers(string[] fullNames, string[] posts, char symbolSeparating, string emptyText)
-        {
-            if (fullNames.Length == 0)
-            {
-                Console.WriteLine(emptyText);
-                return;
-            }
-
-            for (int i = 0; i < fullNames.Length; i++)
-            {
-                Console.Write(symbolSeparating);
-                WriteDossier(i + 1, fullNames[i], posts[i]);
-            }
-
-            Console.WriteLine();
-        }
-
-        private static void WriteDossier(int dossierID, string fullName, string post)
-        {
-            Console.Write(dossierID + " " + fullName + " " + post);
-        }
-
-        private static void FindDossierByLastName(string[] fullNames, string[] posts, char symbolSeparating, string emptyText)
-        {
-            if (fullNames.Length == 0)
-            {
-                Console.WriteLine(emptyText);
-                return;
-            }
-
-            Console.Write("Введите фамилию для поиска: ");
-            string input = Console.ReadLine();
-                 
-            for(int i = 0;i < fullNames.Length;i++)
-            {
-                string[] temp = fullNames[i].Split(symbolSeparating);
-
-                if (temp[0].ToLower() == input.ToLower())
-                {
-                    WriteDossier(i + 1, fullNames[i], posts[i]);
-                    Console.WriteLine();
-                }
-            }
+            Console.SetCursorPosition(playerPositionX, playerPositionY);
+            Console.Write(playerSymbol);
         }
     }
 }
